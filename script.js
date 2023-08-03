65e2ab15205e528e024b6901d0f5ea8afe5466eb
franciscoGimenes <f.gimenes1316@gmail.com>
2023-08-03 14:18:35 -0300

    JS do cilindro

--- script.js
+++ script.js
@@ -27,7 +27,16 @@ function paralelepipedo(){
     let largParalelepipedo = Number(prompt("Qual o valor da largura do paralelepipedo?"))
     let alturaParalelepipedo = Number(prompt("Qual o valor da altura do paralelepipedo"))
     let valorParalelepipedo = ((compParalelepipedo*largParalelepipedo)*alturaParalelepipedo)
-    alert(`o valor do volume da piramide é de ${valorParalelepipedo} m³`)
+    alert(`o valor do volume da paralelepipedo é de ${valorParalelepipedo} m³`)
     textoCone = document.querySelector('h2#paralelepipedo')
     textoCone.textContent = `O valor do volume da paralelepipedo é de ${valorParalelepipedo} m³`
+}
+function cilindro(){
+    const pi = 3.14
+    let raioCilindro = Number(prompt("Qual o valor do raio da base do cilindro?"))
+    let alturaCilindro = Number(prompt("Qual o valor da altura do cilindro?"))
+    let valorCilindro = ((pi*(raioCilindro*raioCilindro))*alturaCilindro)
+    alert(`o valor do volume do cilindro é de ${valorCilindro} m³`)
+    textoCone = document.querySelector('h2#cilindro')
+    textoCone.textContent = `O valor do volume do cilindro é de ${valorCilindro} m³`
 }
