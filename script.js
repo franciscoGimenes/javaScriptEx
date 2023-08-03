4b36cf8f500da6cba2ff2aada1c5d89ef69bf64e
franciscoGimenes <f.gimenes1316@gmail.com>
2023-08-03 13:58:39 -0300

    JS da piramide

--- script.js
+++ script.js
@@ -10,7 +10,15 @@ function cone(){
     let raioCone = Number(prompt("Qual o valor do raio da base do cone?"))
     let alturaCone = Number(prompt("Qual o valor da altura do cone?"))
     let valorCone = ((pi*(raioCone*raioCone))*alturaCone)/3
-    alert(`o valor do volume do cubo é de ${valorCone} m³`)
+    alert(`o valor do volume do cone é de ${valorCone} m³`)
     textoCone = document.querySelector('h2#cone')
     textoCone.textContent = `O valor do volume do cone é de ${valorCone} m³`
+}
+function piramide(){
+    let ladoPiramide = Number(prompt("Qual o valor do lado da base da piramide?"))
+    let alturaPiramide = Number(prompt("Qual o valor da altura da piramide"))
+    let valorPiramide = ((ladoPiramide*ladoPiramide)*alturaPiramide)/3
+    alert(`o valor do volume da piramide é de ${valorPiramide} m³`)
+    textoCone = document.querySelector('h2#piramide')
+    textoCone.textContent = `O valor do volume da piramide é de ${valorPiramide} m³`
 }
