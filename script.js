366b9551e051633fa44f294ea8e752b74685e1c6
franciscoGimenes <f.gimenes1316@gmail.com>
2023-08-03 13:45:30 -0300

    script do cone
    script para calcular o volume do cone

--- script.js
+++ script.js
@@ -1,7 +1,16 @@
 function cubo(){
-    let ladoCubo = prompt("Qual o valor do lado do cubo?")
+    let ladoCubo = Number(prompt("Qual o valor do lado do cubo?"))
     let valorCubo = ladoCubo*ladoCubo*ladoCubo
     alert(`o valor do volume do cubo é de ${valorCubo} m³`)
     textoCubo = document.querySelector('h2.volume')
     textoCubo.textContent = `O valor do volume do cubo é de ${valorCubo} m³`
+}
+function cone(){
+    const pi = 3.14
+    let raioCone = Number(prompt("Qual o valor do raio da base do cone?"))
+    let alturaCone = Number(prompt("Qual o valor da altura do cone?"))
+    let valorCone = ((pi*(raioCone*raioCone))*alturaCone)/3
+    alert(`o valor do volume do cubo é de ${valorCone} m³`)
+    textoCone = document.querySelector('h2.volume')
+    textoCone.textContent = `O valor do volume do cone é de ${valorCone} m³`
 }
